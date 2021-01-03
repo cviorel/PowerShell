50de69449f25ca5b274040ec5efa4f038a07c778
Viorel Ciucu <viorel.ciucu@gmail.com>
2021-01-03 16:29:58 +0100

    I don't get paid enough for this shit.

---
+++ Add-ToPath.ps1
@@ -0,0 +1,20 @@
+﻿
+function Add-ToPath {
+    param(
+        [string]$Directory
+    )
+
+    if ( !(Test-Path $Directory) ) {
+        Write-Warning "$Directory directory was not found!"
+        return
+    }
+    $PATH = [Environment]::GetEnvironmentVariable("PATH")
+
+    if ($Directory.EndsWith('\')) {
+        $Directory = $Directory.Substring(0, $Directory.Length - 1)
+    }
+
+    if ( $PATH -notlike "*" + $Directory + "*" ) {
+        [Environment]::SetEnvironmentVariable("PATH", "$PATH;$Directory", "Machine")
+    }
+}
