c3dc5f8f1d60f53f2482b5ee22ba697d9d998284
Viorel Ciucu <viorel.ciucu@gmail.com>
2021-05-06 10:53:24 +0200

    WIP, always

--- Install-PSModules.ps1
+++ Install-PSModules.ps1
@@ -7,3 +7,4 @@ Install-Module -Name HtmlReport -Scope CurrentUser
 Install-Module -Name PSScriptAnalyzer -Scope CurrentUser
 Install-Module -Name posh-git -Scope CurrentUser
 Install-Module -Name oh-my-posh -Scope CurrentUser
+Install-Module -Name PSWindowsUpdate -Scope CurrentUser
