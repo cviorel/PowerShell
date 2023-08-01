c0d41ecc3b93baf7845638308f1eb66a970a1632
Viorel Ciucu <viorel.ciucu@gmail.com>
2023-08-01 08:51:48 +0200

    making this thing actually usable.

--- Install-Packages.ps1
+++ Install-Packages.ps1
@@ -1,9 +1,7 @@
 #Requires -RunAsAdministrator
 
 winget install -e --id=Microsoft.PowerShell
-winget install -e --id=7zip.7zip
 winget install -e --id=Notepad++.Notepad++
-winget install -e --id=Microsoft.WindowsTerminal
 winget install -e --id=Git.Git
 winget install -e --id=Mozilla.Firefox
 winget install -e --id=Adobe.Acrobat.Reader.64-bit
@@ -16,4 +14,10 @@ winget install -e --id=Zoom.Zoom
 winget install -e --id=SlackTechnologies.Slack
 winget install -e --id=StandardNotes.StandardNotes
 winget install -e --id=Microsoft.VisualStudioCode
+winget install -e --id=Microsoft.AzureDataStudio
+winget install -e --id=7zip.7zip
+winget install -e --id=Microsoft.WindowsTerminal
+winget install -e --id=AutoHotkey.AutoHotkey
+
+#winget install -e --id=XnSoft.XnViewMP
 #winget install -e --id=Microsoft.SQLServerManagementStudio
