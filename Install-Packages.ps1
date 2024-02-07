1f08c138b3b2568632dc04cd1f837bc3e736ebaf
Viorel Ciucu <viorel.ciucu@gmail.com>
2024-02-07 23:29:30 +0100

    added super-widget 2.0.

--- Install-Packages.ps1
+++ Install-Packages.ps1
@@ -1,23 +1,34 @@
 #Requires -RunAsAdministrator
 
-winget install -e --id=Microsoft.PowerShell
-winget install -e --id=Notepad++.Notepad++
-winget install -e --id=Git.Git
+#winget install -e --id=Microsoft.PowerShell
+
 winget install -e --id=Mozilla.Firefox
-winget install -e --id=Adobe.Acrobat.Reader.64-bit
+winget install -e --id=Google.Chrome
 winget install -e --id=JanDeDobbeleer.OhMyPosh
+
 winget install -e --id=Bitwarden.Bitwarden
+
 winget install -e --id=OBSProject.OBSStudio
 winget install -e --id=VideoLAN.VLC
-winget install -e --id=Python.Python.3.11
+
+
 winget install -e --id=Zoom.Zoom
 winget install -e --id=SlackTechnologies.Slack
+winget install -e --id=Microsoft.Teams
+
 winget install -e --id=StandardNotes.StandardNotes
+
+winget install -e --id=Git.Git
+winget install -e --id=Python.Python.3.12
+winget install -e --id=Notepad++.Notepad++
 winget install -e --id=Microsoft.VisualStudioCode
 winget install -e --id=Microsoft.AzureDataStudio
-winget install -e --id=7zip.7zip
-winget install -e --id=Microsoft.WindowsTerminal
-winget install -e --id=AutoHotkey.AutoHotkey
 
+#winget install -e --id=Adobe.Acrobat.Reader.64-bit
+
+
+#winget install -e --id=Microsoft.WindowsTerminal
+#winget install -e --id=AutoHotkey.AutoHotkey
+#winget install -e --id=7zip.7zip
 #winget install -e --id=XnSoft.XnViewMP
 #winget install -e --id=Microsoft.SQLServerManagementStudio
